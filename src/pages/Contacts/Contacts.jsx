4d716744a41bf2381d1fc3ad457406c1d39f3f01
Egor <imd01593@gmail.com>
2023-08-06 17:35:53 +0300

    add form action to contact form

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -2,78 +2,81 @@ const Contacts = () => {
     return (
         <>
             <section className="text-gray-600 body-font relative">
-                <div className="container px-5 py-24 mx-auto">
-                    <div className="flex flex-col text-center w-full mb-12">
-                    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-                        Свяжитесь с нами
-                    </h1>
-                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-                        Отправьте нам заявку
-                    </p>
-                    </div>
-                    <div className="lg:w-1/2 md:w-2/3 mx-auto">
-                    <div className="flex flex-wrap -m-2">
-                        <div className="p-2 w-1/2">
+    <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-12">
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+                Свяжитесь с нами
+            </h1>
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                Отправьте нам заявку
+            </p>
+        </div>
+        <div className="lg:w-1/2 md:w-2/3 mx-auto">
+            <form action="https://formspree.io/f/xleygqdw" method="POST">
+                <div className="flex flex-wrap -m-2">
+                    <div className="p-2 w-1/2">
                         <div className="relative">
                             <label htmlFor="name" className="leading-7 text-sm text-gray-600">
-                            Ваше имя
+                                Ваше имя
                             </label>
                             <input
-                            type="text"
-                            id="Ваше имя"
-                            name="name"
-                            className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                type="text"
+                                id="name"
+                                name="name"
+                                className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                             />
                         </div>
-                        </div>
-                        <div className="p-2 w-1/2">
+                    </div>
+                    <div className="p-2 w-1/2">
                         <div className="relative">
                             <label htmlFor="email" className="leading-7 text-sm text-gray-600">
-                            Ваш Email
+                                Ваш Email
                             </label>
                             <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                type="email"
+                                id="email"
+                                name="email"
+                                className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                             />
                         </div>
-                        </div>
-                        <div className="p-2 w-full">
+                    </div>
+                    <div className="p-2 w-full">
                         <div className="relative">
                             <label
-                            htmlFor="message"
-                            className="leading-7 text-sm text-gray-600"
+                                htmlFor="message"
+                                className="leading-7 text-sm text-gray-600"
                             >
-                            Ваше сообщение
+                                Ваше сообщение
                             </label>
                             <textarea
-                            id="message"
-                            name="message"
-                            className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                            defaultValue={""}
+                                id="message"
+                                name="message"
+                                className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                                defaultValue={""}
                             />
                         </div>
-                        </div>
-                        <div className="p-2 w-full">
-                        <a href="mailto:frontdevegor@gmail.com?subject=Проект&amp;body=Опишите ваш проект:">
-                            <button className="flex mx-auto text-white bg-slate-950 border-0 py-2 px-8 focus:outline-none hover:bg-slate-600 rounded text-lg">
-                                Отправить
-                            </button>
-                        </a>
-                        </div>
-                        <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
-                        <a className="text-slate-950">test@test.com</a>
+                    </div>
+                    <div className="p-2 w-full">
+                        <button
+                            type="submit"
+                            className="flex mx-auto text-white bg-slate-950 border-0 py-2 px-8 focus:outline-none hover:bg-slate-600 rounded text-lg"
+                        >
+                            Отправить
+                        </button>
+                    </div>
+                    <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+                        <a href="mailto:" className="text-slate-950">test@test.com</a>
                         <p className="leading-normal my-5">
                             11K USACHEVA STREET
                             <br />
                             Moscow
                         </p>
-                        </div>
-                    </div>
                     </div>
                 </div>
-            </section>
+            </form>
+        </div>
+    </div>
+</section> 
         </>
 
     )
