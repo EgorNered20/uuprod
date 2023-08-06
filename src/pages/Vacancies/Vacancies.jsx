c9db427bdd6b995c443d6dd83ba7c0dd8ea0a2b3
Egor <imd01593@gmail.com>
2023-08-06 14:52:12 +0300

    Update Vacancies.jsx

--- src/pages/Vacancies/Vacancies.jsx
+++ src/pages/Vacancies/Vacancies.jsx
@@ -5,9 +5,9 @@ const Vacancies = () => {
         <section>
             <div className="container mx-auto mt-32">
                 <h1 className="text-2xl mb-20">Вакансии</h1>
-                <form className='mb-10'>
+                <form className='mb-10' action="https://formsubmit.co/frontdevegor@gmail.com" method="POST">
                     <label>Твоё имя</label>
-                    <input type="text" placeholder="Имя" className='mb-5' />
+                    <input type="text" name='Name' placeholder="Имя" className='mb-5' />
                     <label>Кем ты видишь себя в нашей команде?</label>
                     <label className='mt-5'>
                         <input type="checkbox" name="Test" value="yes"/>Test
@@ -55,13 +55,14 @@ const Vacancies = () => {
                         <input type="checkbox" name="Test" value="yes"/>Test
                     </label>
                     <label className='mt-5'>Телефон</label>
-                    <input type="tel" placeholder="+7 (999) 999-99-99" className='mb-5' />
+                    <input type="tel" name='Phone' placeholder="+7 (999) 999-99-99" className='mb-5' />
+                    <button type='submit' className="flex mx-auto text-white bg-slate-950 border-0 py-2 px-8 focus:outline-none hover:bg-slate-600 rounded text-lg">
+                        Отправить
+                    </button>
                 </form>
-                <a href="mailto:test@gmail.com?subject=Вакансия&amp;body=Кем ты видишь себя в нашей команде:">
-                        <button className="flex mx-auto text-white bg-slate-950 border-0 py-2 px-8 focus:outline-none hover:bg-slate-600 rounded text-lg">
-                            Отправить
-                        </button>
-                </a>
+                {/* <a href="mailto:test@gmail.com?subject=Вакансия&amp;body=Кем ты видишь себя в нашей команде:"> */}
+                        
+                {/* </a> */}
             </div>
         </section>
     )
